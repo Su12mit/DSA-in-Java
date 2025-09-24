d6e01dc9d94fe28b4aa9c1b28d671da0639d199d
Sumit Kumbhar <124146337+Su12mit@users.noreply.github.com>
2025-09-24 20:09:37 +0530

    Adding Solution for  Fibonacci series

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -14,8 +14,12 @@ public class Fibonacci {
     public static void main(String[] args) {
         int a = 0 , b = 1;
         Scanner sc  = new Scanner(System.in);
+        
         System.out.println("Enter the Number for Fibonacci Series => ");
         int n = sc.nextInt();
+        System.out.println("The Fibonacci Series of the number " +n+ " is => ");
+        System.out.println(a);
+        System.out.println(b);
         printfib(a, b, n-2);
     }
 }
