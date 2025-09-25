660941f7f3d4925a787d1807e9b97d3cc7f427a0
Sumit Kumbhar <124146337+Su12mit@users.noreply.github.com>
2025-09-25 19:54:50 +0530

    Adding solution for stack height logn

---
+++ Recursion/StackheightLogn.java
@@ -0,0 +1,24 @@
+public class StackheightLogn {
+    public static int calcpower(int x , int n)
+    {
+        if(n == 0){
+            return 1;
+        }
+        if(x == 0){
+            return 0;
+        }
+        //if even number
+        if(n%2==0){
+            return calcpower(x, n/2)*calcpower(x, n/2);
+              }
+              else{
+                //n is odd
+                return calcpower(x, n/2)*calcpower(x, n/2)*x;
+              }
+    }
+    public static void main(String[] args) {
+        int x = 2, n=5;
+        int ans = calcpower(x, n);
+        System.out.println(ans);
+    }
+}
